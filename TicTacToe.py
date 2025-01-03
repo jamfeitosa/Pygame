7945bbfb8586f78ce4a323d730733d7040681d5c
Jamille <jamfeitosa@gmail.com>
2025-01-03 19:14:05 -0300

    final_version

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import time
 
 pygame.init()
 
@@ -8,6 +9,7 @@ width = 400
 height = 500
 line_color = (120,100,150)
 screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Tic Tac Toe")
 
 # Board 400x400
 board_width = 400
@@ -19,7 +21,13 @@ bh3 = board_height/3
 line_weight = 5
 mark_weight = 3
 mark_color = (0,255,0)
+font = pygame.font.SysFont("Arial", 15)
+font2 = pygame.font.SysFont("Arial", 50, bold=True)
+
 # initializations
+win = False
+draw = False
+current_player = 1
 click = 1
 n_clicks = 0
 cell_array = []
@@ -43,23 +51,30 @@ def draw_o(row,col):
 def cell_update(pos):
     global click
     global n_clicks
-    # find row, col
-    row = math.floor(pos[1]/(bh3))
-    col = math.floor(pos[0]/(bw3))
-    # update cell array
-     
-    if cell_array[row][col] == 0:
-        cell_array[row][col] = click
-        if click == 1:
-            draw_x(row,col)
-        else:
-            draw_o(row,col)
-        click *= -1
-        n_clicks += 1
-    else: 
-        print('try again')
-
+    if pos[1]>board_height:
+        global current_player
+        current_player *= -1
+        return
+    else:
+        # find row, col
+        row = math.floor(pos[1]/(bh3))
+        col = math.floor(pos[0]/(bw3))
+        # update cell array
+        
+        if cell_array[row][col] == 0:
+            cell_array[row][col] = click
+            if click == 1:
+                draw_x(row,col)
+            else:
+                draw_o(row,col)
+            click *= -1
+            n_clicks += 1
+        else: 
+            print('try again')
+   
 def game_status():
+    global current_player
+    global win, draw
     main_diagonal_sum = 0
     sec_diagonal_sum = 0
     for i in range(3):
@@ -68,12 +83,12 @@ def game_status():
     
     if (abs(main_diagonal_sum) == 3):
         pygame.draw.line(screen, (255,0,0), (15,15),(board_width-15, board_height-15), 6)
-        return print('player wins')
-        
+        win = True
+                
     elif abs(sec_diagonal_sum) == 3:
         pygame.draw.line(screen, (255,0,0), (board_width-15,15),(15, board_height-15), 6)
-        return print('player wins')
-
+        win = True
+        
     for col in range(3):
         col_sum = 0
         for row in range(3):
@@ -82,21 +97,94 @@ def game_status():
             
             if abs(row_sum) == 3:
                 pygame.draw.line(screen, (255,0,0), (15,(2*(row+1)-1)*bh3/2),(board_width-15,(2*(row+1)-1)*bh3/2), 6)
-                return print('player wins')
+                win = True
             elif abs(col_sum) == 3:
                 pygame.draw.line(screen, (255,0,0), ((2*(col+1)-1)*bw3/2, 15),((2*(col+1)-1)*bw3/2,board_height-15), 6)
-                return print('player wins')
-    
+                win = True
+                    
     if n_clicks >= 9:
-        return print('player wins')
-
+        draw = True
+  
+def player_turn(current_player, win):
+    global font
+    
+    if win or draw:
         
-draw_grid()
+        return 
+    else:
+        pygame.draw.rect(screen, (0,0,0), (0, board_height,width,height-board_height))
+        if current_player == 1:
+            
+            # player 1 - bold
+            font.set_bold(True)
+            player1 = font.render('Player X', True, (0,255,0))
+            position1 = (15, board_height+20)
+            text = screen.blit(player1, position1)
+            
+            # player 2 - faint
+            font.set_bold(False)
+            player2 = font.render('Player O', True, (0,255,0))
+            position2 = (15, board_height+50)  
+            screen.blit(player2, position2)
+        else:
+            # player 2 - bold
+            font.set_bold(True)
+            player2 = font.render('Player O', True, (0,255,0))
+            position2 = (15, board_height+50)
+            screen.blit(player2, position2)
+            # player 1 - faint
+            font.set_bold(False)
+            player1 = font.render('Player x', True, (0,255,0))
+            position1 = (15, board_height+20)
+            screen.blit(player1, position1)
+    
+def game_result(current_player):
+    global font2, win, draw
+    pygame.draw.rect(screen, (0,0,0), (0, board_height,width,height-board_height))
+    if win:
+        if current_player == 1:
+            text = font2.render("Player X wins!", True, (0,255,0))
+            position = (30, 395)
+            screen.blit(text, position)
+            
+        else:
+            text = font2.render("Player O wins!", True, (0,255,0))
+            position = (30, 395)
+            screen.blit(text, position)
+        
+    elif draw:
+        text = font2.render("Game Draw!", True, (255,0,0))
+        position = (50, 395)
+        screen.blit(text, position)
+        
+    
+    return win, draw
 
+def restart_game():
+    global win, draw, current_player, click, n_clicks, cell_array
+    # initializations
+    time.sleep(3)
+    screen = pygame.display.set_mode((width,height))
+    win = False
+    draw = False
+    current_player = 1
+    click = 1
+    n_clicks = 0
+    cell_array = []
+    for x in range(3):
+        row = [0]*3
+        cell_array.append(row)
+    
+    
+    draw_grid()
+    player_turn(current_player,win)
+
+draw_grid()
+player_turn(current_player,win)
 # Game loop
 run = True
 while run:
-    
+        
     # Event handllers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -105,9 +193,16 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             cell_update(pos)
-            
+                      
         if event.type == pygame.MOUSEBUTTONUP:
             game_status()
-            
+            game_result(current_player)
+            if win or draw:
+                pygame.display.update()
+                restart_game()
+            else:
+                current_player *= -1
+                player_turn(current_player,win)
+
     pygame.display.update()
 pygame.quit()
